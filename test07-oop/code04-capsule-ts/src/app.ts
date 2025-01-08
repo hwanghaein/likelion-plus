c45d6874ec79060f009f34f8feb9587ff3ec63fa
황혜인 <hhi9037@naver.com>
2025-01-08 14:08:20 +0900

    리터럴 타입

--- test07-oop/code04-capsule-ts/src/app.ts
+++ test07-oop/code04-capsule-ts/src/app.ts
@@ -1,4 +1,4 @@
-let msg : unknown = "hello"; // any와는 다르게 unknown은 조건 검사를 함 // 여러 형식 담을 때는 unknown 써서 검사해야함. 타입의 기능을 쓸때는 형식을 비교하고 써야함.
+let msg: unknown = "hello"; // any와는 다르게 unknown은 조건 검사를 함 // 여러 형식 담을 때는 unknown 써서 검사해야함. 타입의 기능을 쓸때는 형식을 비교하고 써야함.
 
 // 조건 검사하는게 불편하면, 타입 단언 사용 (형변환한다고 생각하면됨)
 
@@ -12,9 +12,24 @@ value = "abc"  // 가능
 // value = false // 에러
 
 type ID = number | undefined;
-const a : ID = 1234; // 가능
+const a: ID = 1234; // 가능
 
 // const b : ID = "abc" // 에러
+
+let x: "hi" = "hi";
+x = "hi"
+// x = "no" // 에러 
+
+let level: 1 | 2 | 3 | 4;
+level = 1;
+// level = 6; // 에러
+
+// 담아야하는 값을 나열한다는 점에서는 enum과 겹침
+// 근데 enum은 명명된 "상수"로 한정함
+// 상수형 변수를 나열함
+ 
+
+
 
 type User = {
   name: string;
