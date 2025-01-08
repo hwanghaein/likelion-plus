4f1d4ccb8b2615a07605e54c5d9c505a8ed0bd15
황혜인 <hhi9037@naver.com>
2025-01-08 11:15:27 +0900

    타입 지정하기

--- test07-oop/code04-capsule-ts/src/app.ts
+++ test07-oop/code04-capsule-ts/src/app.ts
@@ -1,3 +1,14 @@
+let user : {name: string, age: number}
+user = { name: "혜인", age: 29 };
+console.log(user);
+
+
+let nums: number[] = [1, 2, 3, 4, 5];
+// nums.push("hello");
+console.log(nums);
+// nums.push(6);
+
+
 function greet(person, date) {
   console.log(`Hello ${person}, today is ${date}`);
 }
