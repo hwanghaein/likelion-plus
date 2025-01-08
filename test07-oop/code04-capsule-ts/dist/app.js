91abda491445426718761a382a2ddb8faa3ad38a
황혜인 <hhi9037@naver.com>
2025-01-08 11:47:54 +0900

    타입 단언

--- test07-oop/code04-capsule-ts/dist/app.js
+++ test07-oop/code04-capsule-ts/dist/app.js
@@ -1,3 +1,12 @@
+var msg = "hello"; // any와는 다르게 unknown은 조건 검사를 함 
+if (typeof msg === "string")
+    msg.toUpperCase();
+var user = { name: "혜인", age: 29 };
+console.log(user);
+var nums = [1, 2, 3, 4, 5];
+// nums.push("hello");
+console.log(nums);
+// nums.push(6);
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date));
 }
